927844ce7828467e9e7baecb0f8e098ae218c978
zavturandrew <128959817+zavturandrew@users.noreply.github.com>
2023-06-24 23:13:47 +0500

    Исправления
    наименование поля временной таблицы. Динамическая размерность частоты при статусе =4

--- tmp_rfm_frequency.sql
+++ tmp_rfm_frequency.sql
@@ -1,26 +1,19 @@
 drop table  if exists analysis.tmp_rfm_frequency;
 CREATE TABLE analysis.tmp_rfm_frequency (
  user_id INT NOT NULL PRIMARY KEY,
- recency INT NOT NULL CHECK(recency >= 1 AND recency <= 5)
+ frequency INT NOT NULL CHECK(frequency >= 1 AND frequency <= 5)
 );
 
-with by_freq_group as (
+
+with CTE as                                              -- Считаем через окошки
+    (
 select user_id,
-ROW_NUMBER() OVER (order by count(order_id)) AS freqs_count
-from analysis.orders
-where status = 4 and date_part('year', order_ts) >= 2022
-group by user_id
-)
+NTILE(5) OVER(ORDER BY recency ASC) AS          recency
+from(
+select user_id,
+SUM(case when status = 4 then 1 else 0 end) AS       recency
+
+from analysis.orders  where date_part('year', order_ts) >= 2022 group by user_id ) AS t
+    )
 insert into analysis.tmp_rfm_frequency
-
-select user_id, case
-
-when freqs_count <= 200 then 1
-when freqs_count <= 400 then 2
-when freqs_count <= 600 then 3
-when freqs_count <= 800 then 4
-when freqs_count <= 1000 then 5
-end
- from by_freq_group;
-
-
+select * from CTE;
